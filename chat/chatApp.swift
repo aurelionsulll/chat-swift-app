188184966f97a008675746cdbd41d2a9e390e305
aurelionsulll <mennanamehdi@gmail.com>
2021-11-25 14:35:41 +0100

    Initial Commit

---
+++ chat/chatApp.swift
@@ -0,0 +1,17 @@
+//
+//  chatApp.swift
+//  chat
+//
+//  Created by mehdi on 25/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct chatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
