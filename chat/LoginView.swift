315fe75778be2290a7b5269d9eb3a1fb52779f77
aurelionsulll <mennanamehdi@gmail.com>
2021-11-26 02:03:40 +0100

    Login register

---
+++ chat/LoginView.swift
@@ -0,0 +1,121 @@
+//
+//  ContentView.swift
+//  chat
+//
+//  Created by mehdi on 25/11/2021.
+//
+
+import SwiftUI
+import Firebase
+
+
+class FirebaseManager: NSObject {
+    let auth: Auth
+    static let shared = FirebaseManager()
+    
+    override init() {
+        FirebaseApp.configure()
+        self.auth = Auth.auth()
+        super.init()
+    }
+}
+
+struct LoginView: View {
+    @State var isLoginMode = false
+    @State var email = ""
+    @State var password = ""
+    @State var loginStatusMessage = ""
+    
+
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                VStack(spacing: 16) {
+                    Picker(selection: $isLoginMode, label: Text("Picker here")) {
+                        Text("Login")
+                            .tag(true)
+                        Text("Create account")
+                            .tag(false)
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                    .padding()
+                    
+                    if !isLoginMode {
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "person.fill")
+                                .font(.system(size: 64))
+                        }
+                    }
+                    
+                    Group {
+                        TextField("Email", text: $email)
+                            .keyboardType(.emailAddress)
+                            .autocapitalization(.none)
+                        
+                        SecureField("Password", text: $password)
+                    }
+                    .padding(12)
+                    .background(Color.white)
+                    
+                    
+                    Button {
+                        handleAction()
+                    } label: {
+                        HStack {
+                            Spacer()
+                            Text(isLoginMode ? "Login" : "Create account")
+                                .foregroundColor(.white)
+                                .padding(.vertical, 10)
+                            Spacer()
+                        }
+                        .background(Color.blue)
+                    }
+                    
+                    Text(self.loginStatusMessage)
+                        .foregroundColor(.red)
+                }
+                .padding()
+            }
+            .navigationTitle(isLoginMode ? "Login" : "Create Account")
+            .background(Color(.init(white:0, alpha:0.05)).ignoresSafeArea())
+        }
+        .navigationViewStyle(StackNavigationViewStyle())
+    }
+    
+    private func handleAction() {
+        if isLoginMode {
+            loginUser()
+        } else {
+            createNewAccount()
+        }
+    }
+        
+    private func createNewAccount() {
+        FirebaseManager.shared.auth.createUser(withEmail: email, password: password) { result, error in
+            if let err = error {
+                self.loginStatusMessage = "Failed to create user: \(err)"
+                return
+            }
+            self.loginStatusMessage = "Successfully created user: \(result?.user.uid ?? "")"
+
+        }
+    }
+    
+    private func loginUser() {
+        FirebaseManager.shared.auth.signIn(withEmail: email, password: password) { result, error in
+            if let err = error {
+                self.loginStatusMessage = "Failed to login user: \(err)"
+                return
+            }
+            self.loginStatusMessage = "Successfully logged in \(result?.user.uid ?? "")"
+        }
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
